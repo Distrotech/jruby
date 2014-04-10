9888a0c3a36b2068b5810dbd7ccd5196f873e862
Christian Meier <m.kristian@web.de>
2014-04-10 17:58:18 +0100

    bouncy-castle is now vendored with jruby-openssl - which allows to update the version without breaking any old jruby-openssl gems

--- lib/pom.rb
+++ lib/pom.rb
@@ -37,7 +37,6 @@ default_gems =
    ImportedGem.new( 'krypt', 'krypt.version', true ),
    ImportedGem.new( 'krypt-core', 'krypt.version', true ),
    ImportedGem.new( 'krypt-provider-jdk', 'krypt.version', true ),
-   ImportedGem.new( 'bouncy-castle-java', 'bc.version', true )
   ]
 
 project 'JRuby Lib Setup' do
