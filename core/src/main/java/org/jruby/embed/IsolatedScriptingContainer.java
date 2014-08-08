0c2b500598f7f43603ccf1acd6d87f4e8a2715bf
Christian Meier <m.kristian@web.de>
2014-08-08 15:46:20 +0200

    add a few more info the javadocs

--- core/src/main/java/org/jruby/embed/IsolatedScriptingContainer.java
+++ core/src/main/java/org/jruby/embed/IsolatedScriptingContainer.java
@@ -14,6 +14,27 @@ import java.net.URL;
  * the root of the "main" classloader is add to LOAD_PATH and GEM_PATH.
  *
  * in the OSGi case there are helper methods to add ClassLoaders to the LOAD_PATH or GEM_PATH
+ * 
+ * a typical setup for the ContextClassLoader case looks likes this:
+ * <li>LOAD_PATH == [ "uri:classloader:/META-INF/jruby.home/lib/ruby/1.9/site_ruby", 
+ *                    "uri:classloader:/META-INF/jruby.home/lib/ruby/shared",
+ *                    "uri:classloader:/META-INF/jruby.home/lib/ruby/1.9",
+ *                    "uri:classloader:" ]</li>
+ * <li>Gem::Specification.dirs ==  [ "uri:classloader:", "uri:classloader:/META-INF/jruby.home/lib/ruby/gems/shared" ]
+ * here very resource is loaded via <code>Thread.currentTHread.getContextClassLoader().getResourceAsStream(...)</code>
+ * 
+ * a typical setup for OSGi case (one bundle with everything):
+ * <li>LOAD_PATH == [ "uri:bundle://16.0:1/META-INF/jruby.home/lib/ruby/1.9/site_ruby", 
+ *                    "uri:bundle://16.0:1/META-INF/jruby.home/lib/ruby/shared",
+ *                    "uri:bundle://16.0:1/META-INF/jruby.home/lib/ruby/1.9",
+ *                    "uri:bundle://16.0:1" ]</li>
+ * <li>Gem::Specification.dirs ==  [ "uri:bundle://16.0:1", "uri:bundle://16.0:1/META-INF/jruby.home/lib/ruby/gems/shared" ]
+ * other OSGi frameworks use other uris like bundleresource:/16.fwk1661197821. here very resource is loaded via 
+ * <code>new URL( uri )openStream()</code>, i.e. <code>new URL(classloader.getResource().toString()).openStream()</code> has to work for
+ * those classloaders. felix and equinox OSGi framework do work.
+ * 
+ * NOTE: <code>Gem.path</code> is base for determine the <code>Gem::Specification.dirs</code> and <code>Gem::Specification.dirs</code> is
+ * used to find gemspec files of the installed gems.
  */
 public class IsolatedScriptingContainer extends ScriptingContainer {
 
